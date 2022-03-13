5892504c6cb608b30663e28b20891eb5d6c94864
ojoaoroberto <ojoao.roberto3@gmail.com>
2022-03-13 15:46:05 -0300

    atividade presencial

---
+++ atividade_prsencial2.js
@@ -0,0 +1,10 @@
+const alunos = 14;
+for(let x=0 ; x<= alunos; x++)
+if (x==0){
+    console.log("0")
+}else if(x %2 ==0){
+    console.log("O número é par:" + x)
+}
+else{
+    console.log("O número é impar:" + x)
+}
